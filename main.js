dc03dd0f28e2b7232bab052ceea678fb2fcf6fb7
peppe2412 <peppeterrazzano79@gmail.com>
2026-03-20 16:15:16 +0100

    Get Api

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("ciao");
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.querySelector("h1");
+  const buttonRegenerate = document.querySelector("#button-regenerate")
+
+  async function getApi() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      h1.textContent = `${data.slip.advice}`;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  buttonRegenerate.addEventListener("click", getApi)
+
+  getApi();
+});
